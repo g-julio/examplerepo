44a51b66f26e2e91c90e205c8556a62b12a827a1
Julio Suarez <julio@gesture.vip>
2024-08-13 13:07:30 -0400

    chore: emulating g-services-ui

---
+++ .releaserc.js
@@ -0,0 +1,39 @@
+const { releaseRules } = require("semantic-release-npm-github-publish");
+const customTransform = require("semantic-release-npm-github-publish/commit-transform");
+
+/**
+ * Gesture Semantic Release Configuration Factory
+ * @type {import('semantic-release').GlobalConfig} semantic-release configuration object.
+ */
+module.exports = {
+  tagFormat: "v${version}",
+  branches: [
+    "production"
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        draftRelease: true,
+        successComment: false,
+        failTitle: false,
+        failComment: false,
+      }
+    ]
+  ],
+  extends: "semantic-release-npm-github-publish",
+  releaseRules: [
+    ...releaseRules,
+    { scope: "no-release", release: false }
+  ],
+  writerOpts: {
+    transform: (commit, context) => {
+      if (commit?.scope === 'no-release') {
+        return null;
+      }
+      return customTransform(commit, context);
+    }
+  },
+};
